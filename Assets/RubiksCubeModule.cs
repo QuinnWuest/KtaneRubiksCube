89c72fe34740bf9a7e4a7cabd373c230dac1b5c1
Timwi <github@timwi.de>
2017-04-29 18:17:35 +0200

    First commit

---
+++ Assets/RubiksCubeModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Rubik's Cube
+/// Created by Timwi
+/// </summary>
+public class RubiksCubeModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    void Start()
+    {
+        Debug.Log("[Rubik's Cube] Started");
+    }
+
+    void ActivateModule()
+    {
+        Debug.Log("[Rubik's Cube] Activated");
+    }
+}
